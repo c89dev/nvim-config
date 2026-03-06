b142088e70cb2b443c8a86c20641280f9bb783bf
c89dev <snarvei@live.com>
2026-03-06 19:56:45 +0100

    init commit

---
+++ lua/config/conform.lua
@@ -0,0 +1,33 @@
+require("conform").setup({
+  formatters_by_ft = {
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
+    json = { "prettier" },
+  },
+
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+
+  format_on_save = {
+    lsp_format = "fallback",
+    timeout_ms = 500,
+  },
+
+  formatters = {
+    prettier = {
+      prepend_args = {
+        "--tab-width", "4",
+        "--use-tabs", "false",
+        "--config-precedence", "cli-override",
+      },
+    },
+  },
+
+  notify_on_error = true,
+  notify_no_formatters = true,
+})
