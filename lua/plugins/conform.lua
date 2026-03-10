df94de2b6c023becb4dec8fe76d5145fb7355876
c89dev <snarvei@live.com>
2026-03-10 09:42:57 +0100

    better completion

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,37 +1,41 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-          clangd = { "prettier" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        html = { "prettier" },
-        css = { "prettier" },
-        json = { "prettier" },
-      },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			clangd = { "prettier" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			json = { "prettier" },
+		},
 
-      default_format_opts = {
-        lsp_format = "fallback",
-      },
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
 
-      format_on_save = {
-        lsp_format = "fallback",
-        timeout_ms = 500,
-      },
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+		},
 
-      formatters = {
-        prettier = {
-          prepend_args = {
-            "--tab-width", "4",
-            "--use-tabs", "false",
-            "--config-precedence", "cli-override",
-          },
-        },
-      },
+		formatters = {
+			prettier = {
+				prepend_args = {
+					"--tab-width",
+					"4",
+					"--use-tabs",
+					"false",
+					"--config-precedence",
+					"cli-override",
+				},
+			},
+		},
 
-      notify_on_error = true,
-      notify_no_formatters = true,
-    }
+		notify_on_error = true,
+		notify_no_formatters = true,
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,74 +1,71 @@
 return {
 
-  {
-    "williamboman/mason.nvim",
-    opts = {},
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = { "stylua" },
+		},
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "clangd",
-        "ts_ls",
-        "html",
-        "cssls",
-        "eslint",
-        "emmet_ls",
-      },
-      automatic_installation = true,
-    },
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"ts_ls",
+				"html",
+				"cssls",
+				"eslint",
+				"emmet_ls",
+			},
+			automatic_installation = true,
+		},
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local caps = require("cmp_nvim_lsp").default_capabilities()
 
-      local caps = require("cmp_nvim_lsp").default_capabilities()
+			local servers = { "clangd", "ts_ls", "html", "cssls", "eslint" }
 
-      local servers = { "clangd", "ts_ls", "html", "cssls", "eslint" }
+			for _, server in ipairs(servers) do
+				vim.lsp.config(server, {
+					capabilities = caps,
+				})
+			end
 
-      for _, server in ipairs(servers) do
-        vim.lsp.config(server, {
-          capabilities = caps,
-        })
-      end
+			vim.lsp.config("emmet_ls", {
+				capabilities = caps,
+				filetypes = {
+					"html",
+					"css",
+					"javascript",
+					"typescript",
+					"javascriptreact",
+					"typescriptreact",
+				},
+			})
 
-      vim.lsp.config("emmet_ls", {
-        capabilities = caps,
-        filetypes = {
-          "html",
-          "css",
-          "javascript",
-          "typescript",
-          "javascriptreact",
-          "typescriptreact",
-        },
-      })
+			vim.lsp.enable({
+				"clangd",
+				"ts_ls",
+				"html",
+				"cssls",
+				"eslint",
+				"emmet_ls",
+			})
 
-      vim.lsp.enable({
-        "clangd",
-        "ts_ls",
-        "html",
-        "cssls",
-        "eslint",
-        "emmet_ls",
-      })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local opts = { buffer = args.buf }
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-
-          local opts = { buffer = args.buf }
-
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-
-        end,
-      })
-
-    end,
-  },
-
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+				end,
+			})
+		end,
+	},
 }
